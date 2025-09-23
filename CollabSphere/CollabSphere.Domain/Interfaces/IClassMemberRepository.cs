8b9c509e601a63e65fcc2a0d84cc8c79275e723c
TriNHM <triminh0502@gmail.com>
2025-09-23 22:11:58 +0700

    [UPDATE] fix namespace of interfaces

--- CollabSphere/CollabSphere.Domain/Interfaces/IClassMemberRepository.cs
+++ CollabSphere/CollabSphere.Domain/Interfaces/IClassMemberRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CollabSphere.Domain.ClassMemberModule.Repos
+namespace CollabSphere.Domain.Intefaces
 {
     internal interface IClassMemberRepository
     {
