e8c9f1106b1a423a281f1d34389e0d42384fa969
TriNHM <triminh0502@gmail.com>
2025-09-24 20:53:59 +0700

    [UPDATE] fix structure of folder Auth

--- CollabSphere/CollabSphere.API/Controllers/AuthController.cs
+++ CollabSphere/CollabSphere.API/Controllers/AuthController.cs
@@ -1,5 +1,5 @@
-﻿using CollabSphere.Application.Auth.Commands;
-using CollabSphere.Application.DTOs.User;
+﻿using CollabSphere.Application.DTOs.User;
+using CollabSphere.Application.Features.Auth.Commands;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
