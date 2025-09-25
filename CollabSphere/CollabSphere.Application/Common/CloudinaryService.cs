dc1e646cbd2dfb356859e2ed1a3a788c151a80e2
TriNHM <triminh0502@gmail.com>
2025-09-25 12:14:48 +0700

    [ADD] add cloudinary service using for upload and download image

---
+++ CollabSphere/CollabSphere.Application/Common/CloudinaryService.cs
@@ -0,0 +1,69 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Common
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; } = string.Empty;
+        public string ApiKey { get; set; } = string.Empty;
+        public string ApiSecret { get; set; } = string.Empty;
+    }
+
+    public class CloudinaryService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public CloudinaryService(IOptions<CloudinarySettings> config)
+        {
+            var settings = config.Value;
+            _cloudinary = new Cloudinary(new Account(
+                settings.CloudName,
+                settings.ApiKey,
+                settings.ApiSecret
+            ));
+            _cloudinary.Api.Secure = true;
+        }
+        public async Task<string> UploadImageAsync(IFormFile file, string folderName)
+        {
+            if (file == null || file.Length == 0)
+                return string.Empty;
+
+            await using var stream = file.OpenReadStream();
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(file.FileName, stream),
+                Folder = folderName
+            };
+
+            var result = await _cloudinary.UploadAsync(uploadParams);
+
+            return result.PublicId.ToString();
+        }
+
+        public string GetImageUrl(string publicId)
+        {
+            return _cloudinary.Api.UrlImgUp.BuildUrl(publicId);
+        }
+
+
+        public async Task<bool> DeleteImageAsync(string publicId)
+        {
+            var deletionParams = new DeletionParams(publicId)
+            {
+                ResourceType = ResourceType.Image
+            };
+
+            var result = await _cloudinary.DestroyAsync(deletionParams);
+            return result.Result == "ok";
+        }
+    }
+}
