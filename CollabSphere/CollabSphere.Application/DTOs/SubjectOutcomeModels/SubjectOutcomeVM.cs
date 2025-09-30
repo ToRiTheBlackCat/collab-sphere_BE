5ff54e77524de4b9af47ef0aa6ab5ace7cd171f9
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-09-30 19:08:14 +0700

    [Add] API - Subject GetAll & GetById
    - Add GetAllSubjects API
    - Add GetSubjectById API

---
+++ CollabSphere/CollabSphere.Application/DTOs/SubjectOutcomeModels/SubjectOutcomeVM.cs
@@ -0,0 +1,28 @@
+﻿using CollabSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.DTOs.SubjectOutcomeModels
+{
+    public class SubjectOutcomeVM
+    {
+        public int SubjectOutcomeId { get; set; }
+
+        public int SyllabusId { get; set; }
+
+        public string OutcomeDetail { get; set; }
+
+        public static implicit operator SubjectOutcomeVM(SubjectOutcome outcome)
+        {
+            return new SubjectOutcomeVM()
+            {
+                SubjectOutcomeId = outcome.SubjectOutcomeId,
+                OutcomeDetail = outcome.OutcomeDetail,
+                SyllabusId = outcome.SyllabusId
+            };
+        }
+    }
+}
