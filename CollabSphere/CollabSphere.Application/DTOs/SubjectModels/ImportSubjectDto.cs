831d6d7e39b0a5d86fc8f7a7af36bb644867eb7c
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-01 13:41:46 +0700

    [Add] API - Import Subjects File
    (cherry picked from commit 3d24dc05bd742ea89d0f97a915c900eb952ed47c)

---
+++ CollabSphere/CollabSphere.Application/DTOs/SubjectModels/ImportSubjectDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.DTOs.SubjectModels
+{
+    public class ImportSubjectDto
+    {
+        public string SubjectName { get; set; }
+
+        public string SubjectCode { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ CollabSphere/CollabSphere.Application/Features/Subjects/Commands/ImportSubject/ImportSubjectCommand.cs
@@ -0,0 +1,15 @@
+﻿using CollabSphere.Application.Base;
+using CollabSphere.Application.DTOs.SubjectModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.Subjects.Commands.ImportSubject
+{
+    public class ImportSubjectCommand : ICommand
+    {
+        public List<ImportSubjectDto> Subjects { get; set; } = new List<ImportSubjectDto>();
+    }
+}
