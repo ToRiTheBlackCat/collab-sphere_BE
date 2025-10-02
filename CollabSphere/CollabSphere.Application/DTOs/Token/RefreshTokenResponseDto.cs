3280d302f3905b118532bee311a1ccfe139bb781
TriNHM <triminh0502@gmail.com>
2025-10-02 11:42:24 +0700

    [ADD] add api refresh token
    Using Redis (Upstash) cache to store

---
+++ CollabSphere/CollabSphere.Application/DTOs/Token/RefreshTokenResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.DTOs.Token
+{
+    public class RefreshTokenResponseDto
+    {
+        public bool IsSuccess { get; set; } = false;
+        public string Message { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
