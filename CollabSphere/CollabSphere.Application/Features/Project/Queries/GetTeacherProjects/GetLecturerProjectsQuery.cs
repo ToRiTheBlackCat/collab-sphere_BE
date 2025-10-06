5559a760bc2c81ef639fa1d3ba5b9285fe19421c
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-06 11:30:48 +0700

    [ADD] API - Get projects of Lecturer

---
+++ CollabSphere/CollabSphere.Application/Features/Project/Queries/GetTeacherProjects/GetLecturerProjectsQuery.cs
@@ -0,0 +1,19 @@
+﻿using CollabSphere.Application.Base;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.Project.Queries.GetTeacherProjects
+{
+    public class GetLecturerProjectsQuery : IQuery<GetLecturerProjectResult>
+    {
+        [FromRoute(Name = "lecturerId")]
+        public int LecturerId { get; set; }
+
+        [FromQuery]
+        public HashSet<int> Statuses { get; set; } = new HashSet<int>();
+    }
+}
