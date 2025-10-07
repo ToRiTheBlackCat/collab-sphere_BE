ac9b5d23606abfa21e6d4cfb3208cdf0301aa1dc
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-07 16:13:16 +0700

    [ADD] API - Get Projects of Class
    # Conflicts:
    #	CollabSphere/CollabSphere.API/Controllers/ProjectController.cs

---
+++ CollabSphere/CollabSphere.Application/Features/Project/Queries/GetProjectsOfClass/GetProjectsOfClassResult.cs
@@ -0,0 +1,16 @@
+﻿using CollabSphere.Application.Base;
+using CollabSphere.Application.Common;
+using CollabSphere.Application.DTOs.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.Project.Queries.GetProjectsOfClass
+{
+    public class GetProjectsOfClassResult : QueryResult
+    {
+        public PagedList<ProjectVM>? PagedProjects { get; set; }
+    }
+}
