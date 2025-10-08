44fbf72e89b1127dc6a9085304814fd796493758
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-08 11:06:00 +0700

    [ADD] API - Head Department Get Pending Projects

---
+++ CollabSphere/CollabSphere.Application/Features/Project/Queries/GetPendingProjects/GetPendingProjectsQuery.cs
@@ -0,0 +1,14 @@
+﻿using CollabSphere.Application.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.Project.Queries.GetPendingProjects
+{
+    public class GetPendingProjectsQuery : PaginationQuery, IQuery<GetPendingProjectsResult>
+    {
+        public string Descriptors { get; set; } = string.Empty;
+    }
+}
