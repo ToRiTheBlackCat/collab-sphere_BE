90aadeb6b58429d49deeb305ab86d6bd3d8e30a2
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-08 13:58:37 +0700

    [ADD] Test - Deny Project test

---
+++ CollabSphere/CollabSphere.Test/Projects/DenyProjectTest.cs
@@ -0,0 +1,142 @@
+﻿using CollabSphere.Application;
+using CollabSphere.Application.Constants;
+using CollabSphere.Application.Features.Project.Commands.ApproveProject;
+using CollabSphere.Application.Features.Project.Commands.DenyProject;
+using CollabSphere.Application.Features.Project.Queries.GetPendingProjects;
+using CollabSphere.Domain.Entities;
+using CollabSphere.Domain.Intefaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Test.Projects
+{
+    public class DenyProjectTest
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+        private readonly Mock<IProjectRepository> _projectRepoMock;
+
+        private readonly DenyProjectHandler _handler;
+
+        public DenyProjectTest()
+        {
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _projectRepoMock = new Mock<IProjectRepository>();
+
+            _unitOfWorkMock.Setup(x => x.ProjectRepo).Returns(_projectRepoMock.Object);
+
+            _handler = new DenyProjectHandler(_unitOfWorkMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnSucces_WhenValidRequest()
+        {
+            // Arrange
+            var command = new DenyProjectCommand()
+            {
+                ProjectId = 1
+            };
+
+            var project = new Project()
+            {
+                ProjectId = 1,
+                ProjectName = "Project Name 1",
+                Description = "Description for Project 1",
+                Status = ProjectStatuses.PENDING
+            };
+
+            _projectRepoMock.Setup(x => x.GetById(1)).ReturnsAsync(project);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.True(result.IsValidInput);
+
+            _unitOfWorkMock.Verify(x => x.ProjectRepo.Update(It.Is<Project>(x => x.ProjectName.Equals("Project Name 1") && x.Status == ProjectStatuses.DENIED)), Times.Once);
+            _unitOfWorkMock.Verify(x => x.CommitTransactionAsync(), Times.Once);
+            Assert.Contains("'Project Name 1' denied", result.Message, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnRequestError_WhenProjectNotFound()
+        {
+            // Arrange
+            var command = new DenyProjectCommand()
+            {
+                ProjectId = 1
+            };
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.False(result.IsValidInput);
+            Assert.Single(result.ErrorList);
+            Assert.Contains(result.ErrorList, x => x.Message.Contains("No Project with ID"));
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnRequestError_WhenProjectIsNotPending()
+        {
+            // Arrange
+            var command = new DenyProjectCommand()
+            {
+                ProjectId = 1
+            };
+
+            var project = new Project()
+            {
+                ProjectId = 1,
+                ProjectName = "Project Name 1",
+                Description = "Description for Project 1",
+                Status = ProjectStatuses.APPROVED
+            };
+
+            _projectRepoMock.Setup(x => x.GetById(1)).ReturnsAsync(project);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.False(result.IsValidInput);
+            Assert.Single(result.ErrorList);
+            Assert.Contains(result.ErrorList, x => x.Message.Contains("not Pending"));
+        }
+
+
+        [Fact]
+        public async Task Handle_ShouldRollBackTransaction_WhenExceptionOccur()
+        {
+            // Arrange
+            var command = new DenyProjectCommand()
+            {
+                ProjectId = 1
+            };
+
+            var project = new Project()
+            {
+                ProjectId = 1,
+                ProjectName = "Project Name 1",
+                Description = "Description for Project 1",
+                Status = ProjectStatuses.APPROVED
+            };
+
+            _projectRepoMock.Setup(x => x.GetById(1)).ThrowsAsync(new Exception("DB Exception"));
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.True(result.IsValidInput);
+            Assert.Equal("DB Exception", result.Message);
+        }
+    }
+}
