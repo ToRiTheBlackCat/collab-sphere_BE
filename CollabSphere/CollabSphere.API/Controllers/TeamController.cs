a1c23c55baf14c1562ef3af8986609bd514f1c10
TriNHM <triminh0502@gmail.com>
2025-10-09 23:32:52 +0700

    [ADD] add api create team

---
+++ CollabSphere/CollabSphere.API/Controllers/TeamController.cs
@@ -0,0 +1,45 @@
+﻿using CollabSphere.Application.DTOs.Lecturer;
+using CollabSphere.Application.DTOs.Team;
+using CollabSphere.Application.Features.Lecturer.Commands;
+using CollabSphere.Application.Features.Team.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollabSphere.API.Controllers
+{
+    [Route("api/team")]
+    [ApiController]
+    public class TeamController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TeamController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTeam ([FromBody] CreateTeamCommand command)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _mediator.Send(command);
+
+            if (!result.IsValidInput)
+            {
+                return BadRequest(result);
+            }
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result.Message);
+            }
+
+            return Ok(result);
+        }
+    }
+}
