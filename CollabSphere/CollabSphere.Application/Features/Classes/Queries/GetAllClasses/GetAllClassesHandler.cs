c6b96536e2c4eb326cf59e1e3f634c7863deae7a
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-10 16:57:13 +0700

    [ADD] APIs - Get classes
    - Get class by ID
    - Get All classes
    - Get lecuter's classes
    - Get student's classes

---
+++ CollabSphere/CollabSphere.Application/Features/Classes/Queries/GetAllClasses/GetAllClassesHandler.cs
@@ -0,0 +1,61 @@
+﻿using CollabSphere.Application.Base;
+using CollabSphere.Application.Common;
+using CollabSphere.Application.DTOs.Classes;
+using CollabSphere.Application.DTOs.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.Classes.Queries.GetAllClasses
+{
+    public class GetAllClassesHandler : QueryHandler<GetAllClassesQuery, GetAllClassesResult>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAllClassesHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        protected override async Task<GetAllClassesResult> HandleCommand(GetAllClassesQuery request, CancellationToken cancellationToken)
+        {
+            var result = new GetAllClassesResult()
+            {
+                IsSuccess = false,
+                IsValidInput = true,
+                Message = string.Empty
+            };
+
+            try
+            {
+                var classes = await _unitOfWork.ClassRepo.SearchClasses(
+                    className: request.ClassName,
+                    lecturerIds: request.LecturerIds,
+                    subjectIds: request.SubjectIds,
+                    orderby: request.OrderBy,
+                    descending: request.Descending);
+
+                result.PaginatedClasses = new PagedList<ClassVM>(
+                    list: classes.Select(x => (ClassVM)x),
+                    pageNum: request.PageNum,
+                    pageSize: request.PageSize,
+                    viewAll: request.ViewAll
+                    );
+                result.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+            }
+
+            return result;
+        }
+
+        protected override async Task ValidateRequest(List<OperationError> errors, GetAllClassesQuery request)
+        {
+            return;
+        }
+    }
+}
---
+++ CollabSphere/CollabSphere.Application/Features/Classes/Queries/GetStudentClasses/GetStudentClassesQuery.cs
@@ -0,0 +1,32 @@
+﻿using CollabSphere.Application.Base;
+using CollabSphere.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.Classes.Queries.GetStudentClasses
+{
+    public class GetStudentClassesQuery : PaginationQuery, IQuery<GetStudentClassesResult>
+    {
+        [FromRoute(Name = "studentId")]
+        public int StudentId { get; set; }
+
+        [FromQuery]
+        public string ClassName { get; set; } = string.Empty;
+
+        [FromQuery]
+        public HashSet<int>? SubjectIds { get; set; }
+
+        [FromQuery]
+        public HashSet<int>? LecturerIds { get; set; }
+
+        [FromQuery]
+        public string OrderBy { get; set; } = nameof(Class.ClassName);
+
+        [FromQuery]
+        public bool Descending { get; set; } = false;
+    }
+}
