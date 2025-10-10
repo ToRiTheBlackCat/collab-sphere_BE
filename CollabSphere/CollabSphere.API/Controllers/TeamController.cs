0774b08150ffd594916cd772770e175f19fa6a1f
TriNHM <triminh0502@gmail.com>
2025-10-10 16:29:57 +0700

    fix conflict

--- CollabSphere/CollabSphere.API/Controllers/TeamController.cs
+++ CollabSphere/CollabSphere.API/Controllers/TeamController.cs
@@ -1,17 +1,9 @@
-﻿using CollabSphere.Application.Features.Project.Commands.DenyProject;
 using CollabSphere.Application.Features.Team.Commands;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-﻿using CollabSphere.Application.DTOs.Lecturer;
-using CollabSphere.Application.Features.Lecturer.Commands;
-using CollabSphere.Application.Features.Team.Commands;
-using MediatR;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace CollabSphere.API.Controllers
 {
@@ -58,7 +50,7 @@ namespace CollabSphere.API.Controllers
                 return BadRequest("Team ID in the URL does not match Team ID in the body.");
             }
 
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
@@ -70,6 +62,35 @@ namespace CollabSphere.API.Controllers
             command.UserRole = int.Parse(roleClaim.Value);
 
             var result = await _mediator.Send(command);
+
+            if (!result.IsValidInput)
+            {
+                return BadRequest(result);
+            }
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result.Message);
+            }
+
+            return Ok(result);
+        }
+        [Authorize]
+        [HttpDelete("{teamId}")]
+        public async Task<IActionResult> DeleteTeam(DeleteTeamCommand command, CancellationToken cancellationToken = default!)
+        {
+            // Get UserId & Role of requester
+            var UIdClaim = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier);
+            var roleClaim = User.Claims.First(c => c.Type == ClaimTypes.Role);
+            command.UserId = int.Parse(UIdClaim.Value);
+            command.UserRole = int.Parse(roleClaim.Value);
+
+            var result = await _mediator.Send(command);
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result);
+            }
 
             if (!result.IsValidInput)
             {
