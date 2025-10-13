48dbe9db1abfc48b14849e91e0e48d5204758f1b
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-13 18:00:48 +0700

    [ADD] API - Create Project

---
+++ CollabSphere/CollabSphere.Application/DTOs/Project/CreateProjectDTO.cs
@@ -0,0 +1,46 @@
+﻿using CollabSphere.Application.Constants;
+using CollabSphere.Application.DTOs.Objective;
+using CollabSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.DTOs.Project
+{
+    public class CreateProjectDTO
+    {
+        [Required]
+        [Length(3, 200)]
+        public string ProjectName { get; set; } = string.Empty;
+
+        [Required]
+        [Length(3, 500)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public int LecturerId { get; set; }
+
+        [Required]
+        public int SubjectId { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public List<CreateProjectObjectiveDTO> Objectives { get; set; } = new List<CreateProjectObjectiveDTO>();
+
+        public Domain.Entities.Project ToProjectEntity()
+        {
+            return new Domain.Entities.Project()
+            {
+                ProjectName = this.ProjectName,
+                Description = this.Description,
+                LecturerId = this.LecturerId,
+                SubjectId = this.SubjectId,
+                Status = ProjectStatuses.PENDING,
+                Objectives = this.Objectives.Select(x => x.ToObjectiveEntity()).ToList(),
+            };
+        }
+    }
+}
