cbfe5c467853a6ec8875805478256f948236169e
TriNHM <triminh0502@gmail.com>
2025-10-14 19:42:47 +0700

    [UPDATE] update api DeleteTeam
    fix logic for updating data in classmember after delete team
    
    # Conflicts:
    #	CollabSphere/CollabSphere.Domain/Interfaces/IClassMemberRepository.cs
    #	CollabSphere/CollabSphere.Infrastructure/Repositories/ClassMemberRepositiory.cs

--- CollabSphere/CollabSphere.Infrastructure/Repositories/ClassMemberRepositiory.cs
+++ CollabSphere/CollabSphere.Infrastructure/Repositories/ClassMemberRepositiory.cs
@@ -32,5 +32,21 @@ namespace CollabSphere.Infrastructure.Repositories
                 .Where(x => x.ClassId == classId)
                 .ToListAsync();
         }
+
+        public async Task<ClassMember?> GetClassMemberAsyncByClassIdAndStudentId(int classId, int studentId)
+        {
+            return await _context.ClassMembers
+                .AsNoTracking()
+                .Where(x => x.ClassId == classId && x.StudentId == studentId)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<ClassMember>?> GetClassMemberAsyncByTeamId(int teamId)
+        {
+            return await _context.ClassMembers
+                .AsNoTracking()
+                .Where(x => x.TeamId == teamId)
+                .ToListAsync();
+        }
     }
 }
