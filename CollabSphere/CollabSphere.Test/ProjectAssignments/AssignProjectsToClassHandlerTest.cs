b6fb2cfc040aa6fb01d7c71513aa026b684fb2ed
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-16 17:49:21 +0700

    [Update] Update test for AssignProjectToClass

--- CollabSphere/CollabSphere.Test/ProjectAssignments/AssignProjectsToClassHandlerTest.cs
+++ CollabSphere/CollabSphere.Test/ProjectAssignments/AssignProjectsToClassHandlerTest.cs
@@ -1,4 +1,5 @@
 ﻿using CollabSphere.Application;
+using CollabSphere.Application.Constants;
 using CollabSphere.Application.Features.ProjectAssignments.Commands.AssignProjectsToClass;
 using CollabSphere.Domain.Entities;
 using CollabSphere.Domain.Intefaces;
@@ -16,6 +17,7 @@ namespace CollabSphere.Test.ProjectAssignments
         private readonly Mock<IUnitOfWork> _unitOfWorkMock;
         private readonly Mock<IProjectRepository> _projectRepoMock;
         private readonly Mock<IProjectAssignmentRepository> _assignmentRepoMock;
+        private readonly Mock<ILecturerRepository> _lectureRepoMock;
         private readonly Mock<IClassRepository> _classRepoMock;
 
         private readonly AssignProjectsToClassHandler _handler;
@@ -25,10 +27,12 @@ namespace CollabSphere.Test.ProjectAssignments
             _unitOfWorkMock = new Mock<IUnitOfWork>();
             _projectRepoMock = new Mock<IProjectRepository>();
             _assignmentRepoMock = new Mock<IProjectAssignmentRepository>();
+            _lectureRepoMock = new Mock<ILecturerRepository>();
             _classRepoMock = new Mock<IClassRepository>();
 
             _unitOfWorkMock.Setup(x => x.ProjectRepo).Returns(_projectRepoMock.Object);
             _unitOfWorkMock.Setup(x => x.ProjectAssignmentRepo).Returns(_assignmentRepoMock.Object);
+            _unitOfWorkMock.Setup(x => x.LecturerRepo).Returns(_lectureRepoMock.Object);
             _unitOfWorkMock.Setup(x => x.ClassRepo).Returns(_classRepoMock.Object);
 
             _handler = new AssignProjectsToClassHandler(_unitOfWorkMock.Object);
@@ -39,7 +43,9 @@ namespace CollabSphere.Test.ProjectAssignments
             return new AssignProjectsToClassCommand()
             {
                 ClassId = 1,
-                ProjectIds = new HashSet<int> { 12, 22 }
+                ProjectIds = new HashSet<int> { 12, 22 },
+                UserId = 2,
+                Role = RoleConstants.LECTURER
             };
         }
 
@@ -49,22 +55,34 @@ namespace CollabSphere.Test.ProjectAssignments
             // Arrange
             var command = GenerateValidCommand();
 
-            _classRepoMock.Setup(x => x.GetById(1)).ReturnsAsync(new Domain.Entities.Class() { ClassId = 1 });
+            var existingAssignment = new ProjectAssignment()
+            {
+                ProjectAssignmentId = 2,
+                ProjectId = 22,
+                ClassId = 1,
+            };
+            var removedAssignment = new ProjectAssignment()
+            {
+                ProjectAssignmentId = 3,
+                ProjectId = 13,
+                ClassId = 1,
+            };
+
+            _classRepoMock.Setup(x => x.GetById(1)).ReturnsAsync(new Domain.Entities.Class() { ClassId = 1, LecturerId = 2, ProjectAssignments = { existingAssignment, removedAssignment } });
+            _lectureRepoMock.Setup(x => x.GetById(2)).ReturnsAsync(new Domain.Entities.Lecturer() { LecturerId = 2 });
             _projectRepoMock.Setup(x => x.GetById(12)).ReturnsAsync(new Domain.Entities.Project() { ProjectId = 12, Status = 1 });
-            _projectRepoMock.Setup(x => x.GetById(22)).ReturnsAsync(new Domain.Entities.Project() { ProjectId = 22, Status = 1 });
-            _assignmentRepoMock.Setup(x => x.GetProjectAssignmentsByClassAsync(1)).ReturnsAsync(new List<Domain.Entities.ProjectAssignment>());
+            _assignmentRepoMock.Setup(x => x.GetProjectAssignmentsByClassAsync(1)).ReturnsAsync(new List<Domain.Entities.ProjectAssignment>() { existingAssignment, removedAssignment });
 
             // Act
             var result = await _handler.Handle(command, CancellationToken.None);
 
             // Assert
-            Assert.True(result.IsSuccess);
             Assert.True(result.IsValidInput);
+            Assert.True(result.IsSuccess);
 
             _unitOfWorkMock.Verify(x => x.BeginTransactionAsync(), Times.Once);
             _assignmentRepoMock.Verify(x => x.Create(It.Is<ProjectAssignment>(x => x.ProjectId == 12 && x.ClassId == 1)), Times.Once);
-            _assignmentRepoMock.Verify(x => x.Create(It.Is<ProjectAssignment>(x => x.ProjectId == 22 && x.ClassId == 1)), Times.Once);
-            _unitOfWorkMock.Verify(x => x.SaveChangesAsync(), Times.Once);
+            _assignmentRepoMock.Verify(x => x.DeleteById(3), Times.Once);
             _unitOfWorkMock.Verify(x => x.CommitTransactionAsync(), Times.Once);
         }
 
@@ -94,10 +112,23 @@ namespace CollabSphere.Test.ProjectAssignments
         {
             // Arrange
             var command = GenerateValidCommand();
+            command.ProjectIds.Add(13);
 
-            _classRepoMock.Setup(x => x.GetById(1)).ReturnsAsync(new Domain.Entities.Class() { ClassId = 1 });
-            _projectRepoMock.Setup(x => x.GetById(12)).ReturnsAsync(new Domain.Entities.Project() { ProjectId = 12, Status = 1 });
-            //_projectRepoMock.Setup(x => x.GetById(22)).ReturnsAsync(new Domain.Entities.Project() { ProjectId = 22, Status = 1 });
+            var projectAssign1 = new ProjectAssignment()
+            {
+                ProjectAssignmentId = 1,
+                ProjectId = 12,
+                ClassId = 1,
+            };
+            var projectAssign2 = new ProjectAssignment()
+            {
+                ProjectAssignmentId = 2,
+                ProjectId = 22,
+                ClassId = 1,
+            };
+
+            _classRepoMock.Setup(x => x.GetById(1)).ReturnsAsync(new Domain.Entities.Class() { ClassId = 1, LecturerId = 2, ProjectAssignments = { projectAssign1, projectAssign2 } });
+            _lectureRepoMock.Setup(x => x.GetById(2)).ReturnsAsync(new Domain.Entities.Lecturer() { LecturerId = 2 });
             _assignmentRepoMock.Setup(x => x.GetProjectAssignmentsByClassAsync(1)).ReturnsAsync(new List<Domain.Entities.ProjectAssignment>());
 
             // Act
@@ -115,10 +146,24 @@ namespace CollabSphere.Test.ProjectAssignments
         {
             // Arrange
             var command = GenerateValidCommand();
+            command.ProjectIds.Add(13);
 
-            _classRepoMock.Setup(x => x.GetById(1)).ReturnsAsync(new Domain.Entities.Class() { ClassId = 1 });
-            _projectRepoMock.Setup(x => x.GetById(12)).ReturnsAsync(new Domain.Entities.Project() { ProjectId = 12, Status = 1 });
-            _projectRepoMock.Setup(x => x.GetById(22)).ReturnsAsync(new Domain.Entities.Project() { ProjectId = 22, Status = 0 });
+            var projectAssign1 = new ProjectAssignment()
+            {
+                ProjectAssignmentId = 1,
+                ProjectId = 12,
+                ClassId = 1,
+            };
+            var projectAssign2 = new ProjectAssignment()
+            {
+                ProjectAssignmentId = 2,
+                ProjectId = 22,
+                ClassId = 1,
+            };
+
+            _classRepoMock.Setup(x => x.GetById(1)).ReturnsAsync(new Domain.Entities.Class() { ClassId = 1, LecturerId = 2, ProjectAssignments = { projectAssign1, projectAssign2 } });
+            _projectRepoMock.Setup(x => x.GetById(13)).ReturnsAsync(new Domain.Entities.Project() { ProjectId = 13, Status = 0 });
+            _lectureRepoMock.Setup(x => x.GetById(2)).ReturnsAsync(new Domain.Entities.Lecturer() { LecturerId = 2 });
             _assignmentRepoMock.Setup(x => x.GetProjectAssignmentsByClassAsync(1)).ReturnsAsync(new List<Domain.Entities.ProjectAssignment>());
 
             // Act
@@ -132,16 +177,39 @@ namespace CollabSphere.Test.ProjectAssignments
         }
 
         [Fact]
-        public async Task Handle_ShouldFailValidation_WhenProjectAlreadyAssigned()
+        public async Task Handle_ShouldFailValidation_WhenRemoveProjectAssignedToTeam()
         {
             // Arrange
             var command = GenerateValidCommand();
 
-            _classRepoMock.Setup(x => x.GetById(1)).ReturnsAsync(new Domain.Entities.Class() { ClassId = 1 });
-            _projectRepoMock.Setup(x => x.GetById(12)).ReturnsAsync(new Domain.Entities.Project() { ProjectId = 12, Status = 1 });
-            _projectRepoMock.Setup(x => x.GetById(22)).ReturnsAsync(new Domain.Entities.Project() { ProjectId = 22, Status = 1 });
-            _assignmentRepoMock.Setup(x => x.GetProjectAssignmentsByClassAsync(1))
-                .ReturnsAsync(new List<Domain.Entities.ProjectAssignment>() { new ProjectAssignment() { ClassId = 1, ProjectId = 22 } });
+            var projectAssign1 = new ProjectAssignment()
+            {
+                ProjectAssignmentId = 1,
+                ProjectId = 12,
+                ClassId = 1,
+            };
+            var projectAssign2 = new ProjectAssignment()
+            {
+                ProjectAssignmentId = 2,
+                ProjectId = 22,
+                ClassId = 1,
+            };
+            var removedAssignment = new ProjectAssignment()
+            {
+                ProjectAssignmentId = 3,
+                ProjectId = 13,
+                ClassId = 1,
+            };
+            var team1 = new Domain.Entities.Team()
+            {
+                TeamId = 1,
+                ProjectAssignmentId = 1,
+                ProjectAssignment = removedAssignment,
+            };
+
+            _classRepoMock.Setup(x => x.GetById(1)).ReturnsAsync(new Domain.Entities.Class() { ClassId = 1, LecturerId = 2, ProjectAssignments = { projectAssign1, projectAssign2, removedAssignment }, Teams = { team1 } });
+            _lectureRepoMock.Setup(x => x.GetById(2)).ReturnsAsync(new Domain.Entities.Lecturer() { LecturerId = 2 });
+            _assignmentRepoMock.Setup(x => x.GetProjectAssignmentsByClassAsync(1)).ReturnsAsync(new List<ProjectAssignment>{ projectAssign1, projectAssign2 });
 
             // Act
             var result = await _handler.Handle(command, CancellationToken.None);
@@ -150,7 +218,7 @@ namespace CollabSphere.Test.ProjectAssignments
             Assert.False(result.IsSuccess);
             Assert.False(result.IsValidInput);
             Assert.Single(result.ErrorList);
-            Assert.Contains("is already assigned Project with ID", result.ErrorList.First().Message);
+            Assert.Contains("Can not remove projects that are assigned", result.ErrorList.First().Message);
         }
 
         [Fact]
