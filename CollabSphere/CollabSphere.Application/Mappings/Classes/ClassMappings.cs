85f91e9faa4a32651d168ae32167134f8f3b81c8
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-16 22:48:09 +0700

    [Update] Update Project Fields

---
+++ CollabSphere/CollabSphere.Application/Mappings/Classes/ClassMappings.cs
@@ -0,0 +1,43 @@
+﻿using CollabSphere.Application.DTOs.Classes;
+using CollabSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Mappings.Classes
+{
+    public static class ClassMappings
+    {
+        public static ClassVM ToClassVM(this Class classEntity, bool showEnrolKey = false)
+        {
+            return new ClassVM()
+            {
+                ClassId = classEntity.ClassId,
+                ClassName = classEntity.ClassName,
+                SubjectId = classEntity.Subject.SubjectId,
+                SubjectCode = classEntity.Subject.SubjectCode,
+                SubjectName = classEntity.Subject.SubjectName,
+                LecturerId = classEntity.Lecturer.LecturerId,
+                LecturerCode = classEntity.Lecturer.LecturerCode,
+                LecturerName = classEntity.Lecturer.Fullname,
+                EnrolKey = showEnrolKey ? classEntity.EnrolKey : "",
+                MemberCount = classEntity.MemberCount,
+                TeamCount = classEntity.TeamCount,
+                CreatedDate = classEntity.CreatedDate,
+                IsActive = classEntity.IsActive,
+            };
+        }
+
+        public static List<ClassVM> ToClassVM(this IEnumerable<Class>? classEntityList, bool showEnrolKey = false)
+        {
+            if (classEntityList == null || !classEntityList.Any())
+            {
+                return new List<ClassVM>();
+            }
+
+            return classEntityList.Select(cls => cls.ToClassVM(showEnrolKey)).ToList();
+        }
+    }
+}
