8f78b295bc2ca3ca534b4fbd3b763460d77c935e
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-16 22:49:12 +0700

    [UPDATE] Update Class Dtos

--- CollabSphere/CollabSphere.Application/DTOs/Classes/ClassVM.cs
+++ CollabSphere/CollabSphere.Application/DTOs/Classes/ClassVM.cs
@@ -25,6 +25,8 @@ namespace CollabSphere.Application.DTOs.Classes
 
         public string LecturerName { get; set; } = string.Empty;
 
+        public string EnrolKey { get; set; }
+
         public int? MemberCount { get; set; }
 
         public int? TeamCount { get; set; }
@@ -33,23 +35,24 @@ namespace CollabSphere.Application.DTOs.Classes
 
         public bool IsActive { get; set; }
 
-        public static explicit operator ClassVM(Class classEntity)
-        {
-            return new ClassVM()
-            {
-                ClassId = classEntity.ClassId,
-                ClassName = classEntity.ClassName,
-                SubjectId = classEntity.Subject.SubjectId,
-                SubjectCode = classEntity.Subject.SubjectCode,
-                SubjectName = classEntity.Subject.SubjectName,
-                LecturerId = classEntity.Lecturer.LecturerId,
-                LecturerCode = classEntity.Lecturer.LecturerCode,
-                LecturerName = classEntity.Lecturer.Fullname,
-                MemberCount = classEntity.MemberCount,
-                TeamCount = classEntity.TeamCount,
-                CreatedDate = classEntity.CreatedDate,
-                IsActive = classEntity.IsActive,
-            };
-        }
+        //public static explicit operator ClassVM(Class classEntity)
+        //{
+        //    return new ClassVM()
+        //    {
+        //        ClassId = classEntity.ClassId,
+        //        ClassName = classEntity.ClassName,
+        //        SubjectId = classEntity.Subject.SubjectId,
+        //        SubjectCode = classEntity.Subject.SubjectCode,
+        //        SubjectName = classEntity.Subject.SubjectName,
+        //        LecturerId = classEntity.Lecturer.LecturerId,
+        //        LecturerCode = classEntity.Lecturer.LecturerCode,
+        //        LecturerName = classEntity.Lecturer.Fullname,
+        //        EnrolKey = classEntity.EnrolKey,
+        //        MemberCount = classEntity.MemberCount,
+        //        TeamCount = classEntity.TeamCount,
+        //        CreatedDate = classEntity.CreatedDate,
+        //        IsActive = classEntity.IsActive,
+        //    };
+        //}
     }
 }
