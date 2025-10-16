3ef1b0065fb7c60100e9b1bfc4d3f94eaa84f38d
TriNHM <triminh0502@gmail.com>
2025-10-16 21:54:09 +0700

    [ADD] add api GetTeamDetail

--- CollabSphere/CollabSphere.API/Controllers/TeamController.cs
+++ CollabSphere/CollabSphere.API/Controllers/TeamController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using CollabSphere.Application.Features.Team.Queries.GetStudentTeamByAssignClass;
+using CollabSphere.Application.Features.Team.Queries.GetTeamDetail;
 
 namespace CollabSphere.API.Controllers
 {
@@ -206,6 +207,26 @@ namespace CollabSphere.API.Controllers
                 return BadRequest(ModelState);
             }
 
+            // Get UserId & Role of requester
+            var UIdClaim = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier);
+            var roleClaim = User.Claims.First(c => c.Type == ClaimTypes.Role);
+            query.UserId = int.Parse(UIdClaim.Value);
+            query.UserRole = int.Parse(roleClaim.Value);
+
+            var result = await _mediator.Send(query, cancellationToken);
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result);
+            }
+
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpGet("{teamId}")]
+        public async Task<IActionResult> GetTeamDetailsById(GetTeamDetailQuery query, CancellationToken cancellationToken = default)
+        {
             // Get UserId & Role of requester
             var UIdClaim = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier);
             var roleClaim = User.Claims.First(c => c.Type == ClaimTypes.Role);
