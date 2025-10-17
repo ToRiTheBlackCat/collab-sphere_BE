bf4d1b5a0c61d5c2c12f76f51e492435ce6bb8a8
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-17 17:13:08 +0700

    [ADD] API - Remove project from approved projects
    - Update valid status for project update handler

---
+++ CollabSphere/CollabSphere.Application/Features/Project/Commands/DeleteApprovedProject/DeleteApprovedProjectCommand.cs
@@ -0,0 +1,18 @@
+﻿using CollabSphere.Application.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.Project.Commands.DeleteApprovedProject
+{
+    public class DeleteApprovedProjectCommand : ICommand
+    {
+        public int ProjectId = -1;
+
+        public int UserId = -1;
+
+        public int UserRole = -1;
+    }
+}
