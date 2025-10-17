15516b2eb39df958d08ce41b1775ff1c98204ae9
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-17 15:34:21 +0700

    [ADD] API - Update class info

---
+++ CollabSphere/CollabSphere.Application/Features/Classes/Commands/UpdateClass/UpdateClassCommand.cs
@@ -0,0 +1,17 @@
+﻿using CollabSphere.Application.Base;
+using CollabSphere.Application.DTOs.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.Classes.Commands.UpdateClass
+{
+    public class UpdateClassCommand : ICommand
+    {
+        [Required]
+        public UpdateClassDto ClassDto { get; set; }
+    }
+}
