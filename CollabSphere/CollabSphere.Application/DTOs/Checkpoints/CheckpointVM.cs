d1d801d289e3fc77f6f6d8ba83112eb1f1ae14eb
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-18 16:45:20 +0700

    [ADD] API - Get milestones of team

---
+++ CollabSphere/CollabSphere.Application/DTOs/Checkpoints/CheckpointVM.cs
@@ -0,0 +1,56 @@
+﻿using CollabSphere.Application.Constants;
+using CollabSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.DTOs.Checkpoints
+{
+    public class CheckpointVM
+    {
+        public int CheckpointId { get; set; }
+
+        public int TeamMilestoneId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Complexity { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly DueDate { get; set; }
+
+        public int Status { get; set; }
+
+        public string StatusString => ((CheckpointStatuses)this.Status).ToString();
+
+        public int FileCount { get; set; }
+
+        public List<CheckpointAssignmentVM> CheckpointAssignments { get; set; } = new List<CheckpointAssignmentVM>();
+
+        //public virtual ICollection<CheckpointFile> CheckpointFiles { get; set; } = new List<CheckpointFile>();
+
+        //public virtual ICollection<CheckpointSubmit> CheckpointSubmits { get; set; } = new List<CheckpointSubmit>();
+
+        public static explicit operator CheckpointVM(Checkpoint checkpoint)
+        {
+            return new CheckpointVM()
+            {
+                 CheckpointId = checkpoint.CheckpointId,
+                 TeamMilestoneId = checkpoint.TeamMilestoneId,
+                 Title = checkpoint.Title,
+                 Description = checkpoint.Description,
+                 Complexity = checkpoint.Complexity,
+                 StartDate = checkpoint.StartDate!.Value,
+                 DueDate = checkpoint.DueDate,
+                 Status = checkpoint.Status,
+                 FileCount = checkpoint.CheckpointFiles.Count,
+                 CheckpointAssignments = checkpoint.CheckpointAssignments.Select(assign => (CheckpointAssignmentVM)assign).ToList()
+            };
+        }
+    }
+}
