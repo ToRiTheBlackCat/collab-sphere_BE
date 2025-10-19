3137ec031eaff94cc7e02a25a4b2c3db2440bb5d
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-19 20:24:23 +0700

    [UPDATE] update data casting fields

---
+++ CollabSphere/CollabSphere.Application/DTOs/MilestoneEvaluations/TeamMilestoneEvaluationVM.cs
@@ -0,0 +1,53 @@
+﻿using CollabSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.DTOs.MilestoneEvaluations
+{
+    public class TeamMilestoneEvaluationVM
+    {
+        public int MilestoneId { get; set; }
+
+        public int TeamId { get; set; }
+
+        #region Lecturer Info
+        public int LecturerId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string AvatarImg { get; set; }
+
+        public string LecturerCode { get; set; }
+
+        public string PhoneNumber { get; set; }
+        #endregion
+
+        public decimal Score { get; set; }
+
+        public string Comment { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public static explicit operator TeamMilestoneEvaluationVM(MilestoneEvaluation evaluation)
+        {
+            return new TeamMilestoneEvaluationVM()
+            {
+                MilestoneId = evaluation.MilestoneId,
+                TeamId = evaluation.TeamId,
+
+                LecturerId = evaluation.LecturerId,
+                FullName = evaluation.Lecturer.Fullname,
+                AvatarImg = evaluation.Lecturer.AvatarImg,
+                PhoneNumber = evaluation.Lecturer.PhoneNumber,
+                LecturerCode = evaluation.Lecturer.LecturerCode,
+
+                Score = evaluation.Score!.Value,
+                Comment = evaluation.Comment,
+                CreatedDate = evaluation.CreatedDate!.Value,
+            };
+        }
+    }
+}
---
+++ CollabSphere/CollabSphere.Application/DTOs/MilestoneReturns/TeamMilestoneReturnVM.cs
@@ -0,0 +1,53 @@
+﻿using CollabSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.DTOs.MilestoneReturns
+{
+    public class TeamMilestoneReturnVM
+    {
+        public int MileReturnId { get; set; }
+
+        public int TeamMilestoneId { get; set; }
+
+        #region Student Info
+        public int ClassMemberId { get; set; }
+
+        public int StudentId { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string AvatarImg { get; set; }
+
+        public string StudentCode { get; set; }
+        #endregion
+
+        public string FilePath { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTime SubmitedDate { get; set; }
+
+        public static explicit operator TeamMilestoneReturnVM(MilestoneReturn milestoneReturn)
+        {
+            return new TeamMilestoneReturnVM()
+            {
+                MileReturnId = milestoneReturn.MileReturnId,
+                TeamMilestoneId = milestoneReturn.TeamMilestoneId,
+
+                ClassMemberId = milestoneReturn.ClassMemberId,
+                StudentId = milestoneReturn.ClassMember.Student.StudentId,
+                Fullname = milestoneReturn.ClassMember.Student.Fullname,
+                AvatarImg = milestoneReturn.ClassMember.Student.AvatarImg,
+                StudentCode = milestoneReturn.ClassMember.Student.StudentCode,
+
+                FilePath =milestoneReturn.FilePath,
+                Type = milestoneReturn.Type,
+                SubmitedDate = milestoneReturn.SubmitedDate,
+            };
+        }
+    }
+}
