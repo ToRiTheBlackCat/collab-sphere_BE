ce8e1297ae6babbb0aeeb91e7409d1ab82788d74
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-19 20:24:23 +0700

    [UPDATE] update data casting fields

---
+++ CollabSphere/CollabSphere.Application/DTOs/MilestoneFiles/TeamMilestoneFileVM.cs
@@ -0,0 +1,31 @@
+﻿using CollabSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.DTOs.MilestoneFiles
+{
+    public class TeamMilestoneFileVM
+    {
+        public int FileId { get; set; }
+
+        public int TeamMilstoneId { get; set; }
+
+        public string FilePath { get; set; }
+
+        public string Type { get; set; }
+
+        public static explicit operator TeamMilestoneFileVM(MilestoneFile file)
+        {
+            return new TeamMilestoneFileVM()
+            {
+                FileId = file.FileId,
+                TeamMilstoneId = file.TeamMilstoneId,
+                FilePath = file.FilePath,
+                Type = file.Type,
+            };
+        }
+    }
+}
