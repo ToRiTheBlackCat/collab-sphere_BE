485211289df2d38c20427e8dc2b0a5e277c6e0d2
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-20 19:42:55 +0700

    [ADD] API - Leader check done team milestone

---
+++ CollabSphere/CollabSphere.Application/Features/TeamMilestones/Commands/CheckTeamMilestone/CheckTeamMilestoneCommand.cs
@@ -0,0 +1,19 @@
+﻿using CollabSphere.Application.Base;
+using CollabSphere.Application.DTOs.TeamMilestones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.TeamMilestones.Commands.CheckTeamMilestone
+{
+    public class CheckTeamMilestoneCommand : ICommand
+    {
+        public CheckTeamMilestoneDto CheckDto { get; set; }
+
+        public int UserId { get; set; } = -1;
+
+        public int UserRole { get; set; } = -1;
+    }
+}
