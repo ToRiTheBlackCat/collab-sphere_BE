a33f4bd5c2e0c71eea0e8e7f6c5a7a1d2042fcd0
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-21 11:43:14 +0700

    [UPDATE] UnitOfWork - Add Repositories

---
+++ CollabSphere/CollabSphere.Infrastructure/Repositories/CheckpointRepository.cs
@@ -0,0 +1,24 @@
+﻿using CollabSphere.Domain.Entities;
+using CollabSphere.Domain.Intefaces;
+using CollabSphere.Infrastructure.Base;
+using CollabSphere.Infrastructure.PostgreDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Infrastructure.Repositories
+{
+    public class CheckpointRepository : GenericRepository<Checkpoint>, ICheckpointRepository
+    {
+        public CheckpointRepository(collab_sphereContext context) : base(context)
+        {
+        }
+
+        public Task<Checkpoint> GetCheckpointDetail(int checkpontId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
