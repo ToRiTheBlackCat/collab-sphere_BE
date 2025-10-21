c5305509f16d2c74a54bc925e8b31f345b5f7493
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-21 12:43:09 +0700

    [ADD] API - Get checkpoint detail

---
+++ CollabSphere/CollabSphere.Application/DTOs/Checkpoints/CheckpointDetailDto.cs
@@ -0,0 +1,55 @@
+﻿using CollabSphere.Application.Constants;
+using CollabSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.DTOs.Checkpoints
+{
+    public class CheckpointDetailDto
+    {
+        public int CheckpointId { get; set; }
+
+        public int TeamMilestoneId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Complexity { get; set; }
+
+        public DateOnly? StartDate { get; set; }
+
+        public DateOnly DueDate { get; set; }
+
+        /// <summary>
+        /// 0 - not done , 1 - done
+        /// </summary>
+        public int Status { get; set; }
+
+        public string StatusString => ((CheckpointStatuses)this.Status).ToString();
+
+        public List<CheckpointAssignmentVM> CheckpointAssignments { get; set; } = new List<CheckpointAssignmentVM>();
+
+        public List<CheckpointFileVM> CheckpointFiles { get; set; } = new List<CheckpointFileVM>();
+
+        public static explicit operator CheckpointDetailDto(Checkpoint checkpoint)
+        {
+            return new CheckpointDetailDto()
+            {
+                CheckpointId = checkpoint.CheckpointId,
+                TeamMilestoneId = checkpoint.TeamMilestoneId,
+                Title = checkpoint.Title,
+                Description = checkpoint.Description,
+                Complexity = checkpoint.Complexity,
+                StartDate = checkpoint.StartDate,
+                DueDate = checkpoint.DueDate,
+                Status = checkpoint.Status,
+                CheckpointAssignments = checkpoint.CheckpointAssignments.Select(x => (CheckpointAssignmentVM)x).ToList(),
+                CheckpointFiles = checkpoint.CheckpointFiles.Select(x => (CheckpointFileVM)x).ToList(),
+            };
+        }
+    }
+}
---
+++ CollabSphere/CollabSphere.Application/Features/Checkpoint/Queries/GetCheckpointDetail/GetCheckpointDetailResult.cs
@@ -0,0 +1,15 @@
+﻿using CollabSphere.Application.Base;
+using CollabSphere.Application.DTOs.Checkpoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.Checkpoint.Queries.GetCheckpointDetail
+{
+    public class GetCheckpointDetailResult : QueryResult
+    {
+        public CheckpointDetailDto? Checkpoint { get; set; }
+    }
+}
