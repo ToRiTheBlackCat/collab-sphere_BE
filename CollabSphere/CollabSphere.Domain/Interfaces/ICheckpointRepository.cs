c515e8f5a5afed9de6c9c3e9d28d525d20d680da
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-21 11:43:14 +0700

    [UPDATE] UnitOfWork - Add Repositories

--- CollabSphere/CollabSphere.Domain/Interfaces/ICheckpointRepository.cs
+++ CollabSphere/CollabSphere.Domain/Interfaces/ICheckpointRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CollabSphere.Domain.Entities;
+using CollabSphere.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace CollabSphere.Domain.Intefaces
 {
-    internal interface ICheckpointRepository
+    public interface ICheckpointRepository : IGenericRepository<Checkpoint>
     {
+        Task<Checkpoint> GetCheckpointDetail(int checkpontId);
     }
 }
