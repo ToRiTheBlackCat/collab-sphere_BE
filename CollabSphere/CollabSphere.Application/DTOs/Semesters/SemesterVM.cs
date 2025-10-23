bca3a134774fbfd65971cca73599c508e8b327ed
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-23 12:54:25 +0700

    [ADD] API - get all semesters

---
+++ CollabSphere/CollabSphere.Application/DTOs/Semesters/SemesterVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.DTOs.Semesters
+{
+    public class SemesterVM
+    {
+        public int SemesterId { get; set; }
+
+        public string SemesterName { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+    }
+}
