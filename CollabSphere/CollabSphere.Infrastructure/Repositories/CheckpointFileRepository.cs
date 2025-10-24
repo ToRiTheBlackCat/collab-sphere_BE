6961b0dc2369174edf9da3cdd4d9f54d185b1046
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-24 17:37:35 +0700

    [UPDATE] DeleteCheckpoint - Add file delete logic

--- CollabSphere/CollabSphere.Infrastructure/Repositories/CheckpointFileRepository.cs
+++ CollabSphere/CollabSphere.Infrastructure/Repositories/CheckpointFileRepository.cs
@@ -2,6 +2,7 @@
 using CollabSphere.Domain.Intefaces;
 using CollabSphere.Infrastructure.Base;
 using CollabSphere.Infrastructure.PostgreDbContext;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,16 @@ namespace CollabSphere.Infrastructure.Repositories
     {
         public CheckpointFileRepository(collab_sphereContext context) : base(context)
         {
+        }
+
+        public async Task<List<CheckpointFile>> GetFilesByCheckpointId(int checkpointId)
+        {
+            var query = _context.CheckpointFiles
+                .AsNoTracking()
+                .Where(x => x.CheckpointId == checkpointId)
+                .ToListAsync();
+
+            return await query;
         }
     }
 }
