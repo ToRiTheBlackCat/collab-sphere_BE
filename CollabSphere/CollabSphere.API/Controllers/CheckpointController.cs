68281f78d63ac705a42c8a63ea600a45f456e1f5
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-28 00:10:06 +0700

    [ADD] API - Upload checkpoint file

--- CollabSphere/CollabSphere.API/Controllers/CheckpointController.cs
+++ CollabSphere/CollabSphere.API/Controllers/CheckpointController.cs
@@ -2,11 +2,13 @@
 using CollabSphere.Application.DTOs.Checkpoints;
 using CollabSphere.Application.Features.Checkpoints.Commands.AssignMembersToCheckpoint;
 using CollabSphere.Application.Features.Checkpoints.Commands.CheckDoneCheckpoint;
+using CollabSphere.Application.Features.Checkpoints.Commands.CheckpointUpload;
 using CollabSphere.Application.Features.Checkpoints.Commands.CreateCheckpoint;
 using CollabSphere.Application.Features.Checkpoints.Commands.DeleteCheckpoint;
 using CollabSphere.Application.Features.Checkpoints.Commands.UpdateCheckpoint;
 using CollabSphere.Application.Features.Checkpoints.Queries.GetCheckpointDetail;
 using CollabSphere.Application.Features.TeamMilestones.Commands.CheckTeamMilestone;
+using CollabSphere.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -207,6 +209,40 @@ namespace CollabSphere.API.Controllers
             var command = new DeleteCheckpointCommand()
             {
                 CheckpointId = checkpointId,
+                UserId = int.Parse(UIdClaim.Value),
+                UserRole = int.Parse(roleClaim.Value),
+            };
+
+            // Handle command
+            var result = await _mediator.Send(command, cancellationToken);
+
+            if (!result.IsValidInput)
+            {
+                return BadRequest(result.ErrorList);
+            }
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result.Message);
+            }
+
+            return Ok(result.Message);
+        }
+
+        // Roles: Lecturer, Student
+        [Authorize(Roles = "4, 5")]
+        [HttpPost("{checkpointId}/uploads")]
+        public async Task<IActionResult> UploadFile(int checkpointId, IFormFile checkpointFile, CancellationToken cancellationToken = default)
+        {
+            // Get UserId & Role of requester
+            var UIdClaim = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier);
+            var roleClaim = User.Claims.First(c => c.Type == ClaimTypes.Role);
+
+            // Construct command
+            var command = new CheckpointUploadCommand()
+            {
+                CheckpointId = checkpointId,
+                File = checkpointFile,
                 UserId = int.Parse(UIdClaim.Value),
                 UserRole = int.Parse(roleClaim.Value),
             };
---
+++ CollabSphere/CollabSphere.Application/Features/Checkpoints/Commands/CheckpointUpload/CheckpointUploadCommand.cs
@@ -0,0 +1,21 @@
+﻿using CollabSphere.Application.Base;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.Checkpoints.Commands.CheckpointUpload
+{
+    public class CheckpointUploadCommand : ICommand
+    {
+        public int CheckpointId { get; set; } = -1;
+
+        public IFormFile File { get; set; }
+
+        public int UserId { get; set; } = -1;
+
+        public int UserRole { get; set; } = -1;
+    }
+}
