a30d0ec11fb69689b604acfea7f19d50d854fd6b
TriNHM <triminh0502@gmail.com>
2025-10-30 10:29:46 +0700

    [ADD] add api UpdateMilestoneQuestion

--- CollabSphere/CollabSphere.API/Controllers/QuestionController.cs
+++ CollabSphere/CollabSphere.API/Controllers/QuestionController.cs
@@ -1,4 +1,5 @@
-﻿using CollabSphere.Application.Features.MilestoneQues.Commands;
+﻿using CollabSphere.Application.Features.MilestoneQues.Commands.CreateMilestoneQuestion;
+using CollabSphere.Application.Features.MilestoneQues.Commands.UpdateMilestoneQuestion;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -34,6 +35,38 @@ namespace CollabSphere.API.Controllers
             command.UserId = int.Parse(UIdClaim.Value);
             command.UserRole = int.Parse(roleClaim.Value);
             command.TeamMilestoneId = teamMilestoneId;
+
+            var result = await _mediator.Send(command);
+
+            if (!result.IsValidInput)
+            {
+                return BadRequest(result);
+            }
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result);
+            }
+
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "4")]
+        [HttpPatch("{questionId}")]
+        public async Task<IActionResult> UpdateMilestoneQuestion(int questionId, [FromBody] UpdateMilestoneQuestionCommand command)
+        {
+            if (!ModelState.IsValid)
+
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Get UserId & Role of requester
+            var UIdClaim = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier);
+            var roleClaim = User.Claims.First(c => c.Type == ClaimTypes.Role);
+            command.UserId = int.Parse(UIdClaim.Value);
+            command.UserRole = int.Parse(roleClaim.Value);
+            command.QuestionId = questionId;
 
             var result = await _mediator.Send(command);
 
--- CollabSphere/CollabSphere.Infrastructure/Repositories/MilestoneQuestionRepository.cs
+++ CollabSphere/CollabSphere.Infrastructure/Repositories/MilestoneQuestionRepository.cs
@@ -2,6 +2,7 @@
 using CollabSphere.Domain.Intefaces;
 using CollabSphere.Infrastructure.Base;
 using CollabSphere.Infrastructure.PostgreDbContext;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,12 @@ namespace CollabSphere.Infrastructure.Repositories
     {
         public MilestoneQuestionRepository(collab_sphereContext context) : base(context)
         {
+        }
+
+        public override async Task<MilestoneQuestion?> GetById (int questionId)
+        {
+            return await _context.MilestoneQuestions
+                .SingleOrDefaultAsync(x => x.MilestoneQuestionId == questionId);
         }
     }
 }
