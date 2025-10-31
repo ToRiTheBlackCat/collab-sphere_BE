f36558ae4d34791ade8d815fed12ce27b0d61468
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-10-31 20:58:30 +0700

    [ADD] API - Generate new milestone file URL

--- CollabSphere/CollabSphere.API/Controllers/TeamMilestonesController.cs
+++ CollabSphere/CollabSphere.API/Controllers/TeamMilestonesController.cs
@@ -1,4 +1,6 @@
 ﻿using CollabSphere.Application.DTOs.TeamMilestones;
+using CollabSphere.Application.Features.MilestoneFiles.Commands.DeleteMilestoneFile;
+using CollabSphere.Application.Features.MilestoneFiles.Commands.GenerateMilestoneFileUrl;
 using CollabSphere.Application.Features.MilestoneFiles.Commands.UploadMilestoneFile;
 using CollabSphere.Application.Features.TeamMilestones.Commands.CheckTeamMilestone;
 using CollabSphere.Application.Features.TeamMilestones.Commands.CreateCustomTeamMilestone;
@@ -6,15 +8,10 @@ using CollabSphere.Application.Features.TeamMilestones.Commands.DeleteCustomTeam
 using CollabSphere.Application.Features.TeamMilestones.Commands.UpdateTeamMilestone;
 using CollabSphere.Application.Features.TeamMilestones.Queries.GetMilestoneDetail;
 using CollabSphere.Application.Features.TeamMilestones.Queries.GetMilestonesByTeam;
-using CollabSphere.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-using System.Threading;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace CollabSphere.API.Controllers
 {
@@ -233,7 +230,6 @@ namespace CollabSphere.API.Controllers
             return Ok(result.Message);
         }
 
-
         // Roles: Lecturer
         [Authorize(Roles = "4")]
         [HttpPost("{teamMilestoneId}/files")]
@@ -300,6 +296,44 @@ namespace CollabSphere.API.Controllers
             }
 
             return Ok(result.Message);
+        }
+
+        // Roles: Lecturer, Student
+        [Authorize(Roles = "4, 5")]
+        [HttpPatch("{teamMilestoneId}/files/{fileId}/new-url")]
+        public async Task<IActionResult> GenerateNewFileUrl(int teamMilestoneId, int fileId, CancellationToken cancellationToken = default)
+        {
+            // Get UserId & Role of requester
+            var UIdClaim = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier);
+            var roleClaim = User.Claims.First(c => c.Type == ClaimTypes.Role);
+
+            // Construct command
+            var command = new GenerateMilestoneFileUrlCommand()
+            {
+                TeamMilestoneId = teamMilestoneId,
+                FileId = fileId,
+                UserId = int.Parse(UIdClaim.Value),
+                UserRole = int.Parse(roleClaim.Value),
+            };
+
+            // Handle command
+            var result = await _mediator.Send(command, cancellationToken);
+
+            if (!result.IsValidInput)
+            {
+                return BadRequest(result);
+            }
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result.Message);
+            }
+
+            return Ok(new
+            {
+                result.FileUrl,
+                result.UrlExpireTime
+            });
         }
     }
 }
