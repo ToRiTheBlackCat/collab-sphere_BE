f26cc8f00e04b53c22e579a302ab1b6e6af326a9
TriNHM <triminh0502@gmail.com>
2025-11-06 23:20:42 +0700

    [ADD] createMeetingCommand

---
+++ CollabSphere/CollabSphere.Application/Features/Meeting/Commands/CreateMeeting/CreateMeetingCommand.cs
@@ -0,0 +1,29 @@
+﻿using CollabSphere.Application.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.Meeting.Commands.CreateMeeting
+{
+    public class CreateMeetingCommand : ICommand
+    {
+        [JsonIgnore]
+        public int UserId = -1;
+        [JsonIgnore]
+        public int UserRole = -1;
+
+        [Required]
+        public int TeamId { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        [Required]
+        public DateTime ScheduleTime { get; set; }
+        [Required]
+        public bool IsCallNow { get; set; }
+    }
+}
