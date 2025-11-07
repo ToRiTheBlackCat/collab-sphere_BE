589ee25a7b41ccd0d35ddb53b41800ae6c78605f
TriNHM <triminh0502@gmail.com>
2025-11-07 20:17:37 +0700

    [ADD] add api update meeting

--- CollabSphere/CollabSphere.API/Controllers/MeetingController.cs
+++ CollabSphere/CollabSphere.API/Controllers/MeetingController.cs
@@ -1,4 +1,5 @@
 ﻿using CollabSphere.Application.Features.Meeting.Commands.CreateMeeting;
+using CollabSphere.Application.Features.Meeting.Commands.UpdateMeeting;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -21,6 +22,36 @@ namespace CollabSphere.API.Controllers
         [Authorize]
         [HttpPost]
         public async Task<IActionResult> CreateMeeting([FromBody] CreateMeetingCommand command)
+        {
+            if (!ModelState.IsValid)
+
+            {
+                return BadRequest(ModelState);
+            }
+            // Get UserId & Role of requester
+            var UIdClaim = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier);
+            var roleClaim = User.Claims.First(c => c.Type == ClaimTypes.Role);
+            command.UserId = int.Parse(UIdClaim.Value);
+            command.UserRole = int.Parse(roleClaim.Value);
+
+            var result = await _mediator.Send(command);
+
+            if (!result.IsValidInput)
+            {
+                return BadRequest(result);
+            }
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result.Message);
+            }
+
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpPatch("{meetingId}")]
+        public async Task<IActionResult> UpdateMeeting(UpdateMeetingCommand command)
         {
             if (!ModelState.IsValid)
 
