79b59c6ab207022816a9e95b633092b03d2411de
TriNHM <triminh0502@gmail.com>
2025-11-11 00:30:21 +0700

    [ADD] add api upload video to gg drive
    file drive ở Drive của team

--- CollabSphere/CollabSphere.API/Controllers/AvatarController.cs
+++ CollabSphere/CollabSphere.API/Controllers/AvatarController.cs
@@ -1,6 +1,9 @@
-﻿using CollabSphere.Application.DTOs.Image;
+﻿using Amazon.Runtime.Internal;
+using CollabSphere.Application.DTOs.Image;
 using CollabSphere.Application.Features.User.Commands;
 using CollabSphere.Application.Features.User.Queries;
+using CollabSphere.Application.Features.VideoRecord.Commands.UploadRecord;
+using Google.Apis.Drive.v3;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +27,26 @@ namespace CollabSphere.API.Controllers
             var imageUrl = await _mediator.Send(new GetAvatarUrlQuery(publicId));
 
             return Ok(imageUrl);
+        }
+
+        [HttpPost("/api/video/uploadation")]
+        [RequestSizeLimit(1073741824)]
+        public async Task<IActionResult> UploadVideoRecord(UploadRecordCommand command)
+        {
+           
+            var result = await _mediator.Send(command);
+
+            if (!result.IsValidInput)
+            {
+                return BadRequest(result);
+            }
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result.Message);
+            }
+
+            return Ok(result);
         }
     }
 }
---
+++ CollabSphere/CollabSphere.Application/Features/VideoRecord/Commands/UploadRecord/UploadRecordHandler.cs
@@ -0,0 +1,75 @@
+﻿using CollabSphere.Application.Base;
+using CollabSphere.Application.Common;
+using CollabSphere.Application.DTOs.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace CollabSphere.Application.Features.VideoRecord.Commands.UploadRecord
+{
+    public class UploadRecordHandler : CommandHandler<UploadRecordCommand>
+    {
+        private readonly GgDriveVideoService _ggDriveService;
+        public UploadRecordHandler(GgDriveVideoService ggDriveService)
+        {
+            _ggDriveService = ggDriveService;
+        }
+
+        protected override async Task<CommandResult> HandleCommand(UploadRecordCommand request, CancellationToken cancellationToken)
+        {
+            var result = new CommandResult()
+            {
+                IsSuccess = false,
+                IsValidInput = true,
+                Message = string.Empty
+            };
+
+            try
+            {
+                var videoUrl = await _ggDriveService.UploadFileAsync(request.VideoFile);
+
+                if (videoUrl != null)
+                {
+                    result.IsSuccess = true;
+                    result.Message = videoUrl;
+                }
+                else
+                {
+                    result.Message = "Upload video failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+            }
+
+            return result;
+        }
+
+        protected override async Task ValidateRequest(List<OperationError> errors, UploadRecordCommand request)
+        {
+            if (request.VideoFile == null || request.VideoFile.Length == 0)
+            {
+                errors.Add(new OperationError
+                {
+                    Field = nameof(request.VideoFile),
+                    Message = $"No file upload"
+                });
+                return;
+            }
+
+            if (request.VideoFile.Length > 1024 * 1024 * 1024)
+            {
+                errors.Add(new OperationError
+                {
+                    Field = nameof(request.VideoFile),
+                    Message = "File size cannot exceed 1 GB"
+                });
+                return;
+            }
+        }
+    }
+}
