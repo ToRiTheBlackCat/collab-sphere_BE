2150c4b4c3c0f900517af774cb5937435b390dda
TriNHM <triminh0502@gmail.com>
2025-11-10 14:55:07 +0700

    [ADD] add RenameListCommand

---
+++ CollabSphere/CollabSphere.Application/Features/TeamWorkSpace/Commands/ListCommands/RenameList/RenameListCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Application.Features.TeamWorkSpace.Commands.ListCommands.RenameList
+{
+    public class RenameListCommand
+    {
+        [JsonIgnore]
+        public int WorkSpaceId { get; set; }
+        [JsonIgnore]
+        public int ListId { get; set; }
+        [JsonIgnore]
+        public int RequesterId { get; set; }
+        [Required]
+        public float NewTitle { get; set; }
+    }
+}
