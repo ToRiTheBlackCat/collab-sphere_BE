e59ed87c055707b83cba3f11bf6d42225ec1d80b
TriNHM <triminh0502@gmail.com>
2025-11-12 00:34:50 +0700

    [UPDATE] update function CreateList
    Test OK

--- CollabSphere/CollabSphere.Application/Features/TeamWorkSpace/Commands/ListCommands/CreateList/CreateListCommand.cs
+++ CollabSphere/CollabSphere.Application/Features/TeamWorkSpace/Commands/ListCommands/CreateList/CreateListCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CollabSphere.Application.Base;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace CollabSphere.Application.Features.TeamWorkSpace.Commands.ListCommands.CreateList
 {
-    public class CreateListCommand
+    public class CreateListCommand : ICommand
     {
         [JsonIgnore]
         public int WorkSpaceId { get; set; }
