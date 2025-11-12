4a7b6d9232bc00c36a809dcdbf7e0446b571c013
TriNHM <triminh0502@gmail.com>
2025-11-13 00:21:24 +0700

    [UPDATE] update apis GET/POST ProjectInstallations

--- CollabSphere/CollabSphere.Application/IUnitOfWork.cs
+++ CollabSphere/CollabSphere.Application/IUnitOfWork.cs
@@ -41,7 +41,7 @@ namespace CollabSphere.Application
         IAnswerEvaluationRepository AnswerEvaluationRepo { get; }
         IMilestoneEvaluationRepository MilestoneEvaluationRepo { get; }
         IMilestoneReturnRepository MilestoneReturnRepo { get; }
-        IProjectInstallationRepository ProjectInstallationRepo { get; }
+        IProjectRepoMappingRepository ProjectRepoMappingRepo { get; }
         IMeetingRepository MeetingRepo { get; }
          //More IRepo below
 
---
+++ CollabSphere/CollabSphere.Domain/Interfaces/IProjectRepoMappingRepository.cs
@@ -0,0 +1,14 @@
+﻿using CollabSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabSphere.Domain.Interfaces
+{
+    public interface IProjectRepoMappingRepository : IGenericRepository<ProjectRepoMapping>
+    {
+        Task<Dictionary<int, List<ProjectRepoMapping>>> SearchInstallationsOfProject(int projectId, int installedUserId, int teamId, DateTime? fromDate, bool isDesc);
+    }
+}
