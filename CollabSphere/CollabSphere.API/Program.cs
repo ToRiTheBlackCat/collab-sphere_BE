1abe071f4a667db9821feea5c69f90775397762b
TriNHM <triminh0502@gmail.com>
2025-11-28 14:25:08 +0700

    [UPDATE] add FE domain-fix CORS

--- CollabSphere/CollabSphere.API/Program.cs
+++ CollabSphere/CollabSphere.API/Program.cs
@@ -72,7 +72,8 @@ builder.Services.AddCors(options =>
             "http://127.0.0.1:5500", 
             "http://localhost:5173",
             "http://127.0.0.1:5173",
-            "http://52.221.106.143"
+            "http://52.221.106.143",
+            "https://collabsphere.space/"
         )
         .AllowAnyHeader()
         .AllowAnyMethod()
