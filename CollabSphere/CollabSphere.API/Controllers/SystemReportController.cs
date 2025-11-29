d9317ef3c7858314502c842e521aef03d5d46909
TriNHM <triminh0502@gmail.com>
2025-11-29 11:29:11 +0700

    [ADD] add api CreateSystemReports

---
+++ CollabSphere/CollabSphere.API/Controllers/SystemReportController.cs
@@ -0,0 +1,54 @@
+﻿using CollabSphere.Application.Features.SystemReport.Commands.CreateSystemReport;
+using CollabSphere.Application.Features.Team.Commands.CreateTeam;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace CollabSphere.API.Controllers
+{
+    [Route("api/system-reports")]
+    [ApiController]
+    public class SystemReportController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SystemReportController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> CreateSystemReport([FromForm] CreateSystemReportCommand command)
+        {
+            if (!ModelState.IsValid)
+
+            {
+                return BadRequest(ModelState);
+            }
+            // Get UserId & Role of requester
+            var UIdClaim = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier);
+            var roleClaim = User.Claims.First(c => c.Type == ClaimTypes.Role);
+            command.UserId = int.Parse(UIdClaim.Value);
+
+            var result = await _mediator.Send(command);
+
+            if (!result.IsValidInput)
+            {
+                return BadRequest(result);
+            }
+
+            if (!result.IsSuccess)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result.Message);
+            }
+
+            return Ok(result);
+        }
+
+    }
+}
