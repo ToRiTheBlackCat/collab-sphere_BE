2e9987a0595130f7e2bf579cffabf28274ab0fb9
BaDiMuaSua15-5 <sentientpeas@gmail.com>
2025-12-05 14:04:00 +0700

    [UPDATE] Handler - Update checkpoint info
    Add validation to only allow checkpoint update if checkpoint's milestone is NOT evaluated and NOT marked done

--- CollabSphere/CollabSphere.Application/Features/Checkpoints/Commands/UpdateCheckpoint/UpdateCheckpointHandler.cs
+++ CollabSphere/CollabSphere.Application/Features/Checkpoints/Commands/UpdateCheckpoint/UpdateCheckpointHandler.cs
@@ -66,18 +66,6 @@ namespace CollabSphere.Application.Features.Checkpoints.Commands.UpdateCheckpoin
         {
             var dto = request.UpdateDto;
 
-            // Check checkpointId
-            var checkpoint = await _unitOfWork.CheckpointRepo.GetById(dto.CheckpointId);
-            if (checkpoint == null)
-            {
-                errors.Add(new OperationError()
-                {
-                    Field = nameof(dto.CheckpointId),
-                    Message = $"No checkpoint with ID: {dto.CheckpointId}",
-                });
-                return;
-            }
-
             // Check teamMilesotneId
             var teamMilestone = await _unitOfWork.TeamMilestoneRepo.GetDetailById(dto.TeamMilestoneId);
             if (teamMilestone == null)
@@ -90,30 +78,71 @@ namespace CollabSphere.Application.Features.Checkpoints.Commands.UpdateCheckpoin
                 return;
             }
 
-            // Check if user is team member
-            if (request.UserRole == RoleConstants.STUDENT)
+            // Check checkpointId
+            var checkpoint = await _unitOfWork.CheckpointRepo.GetById(dto.CheckpointId);
+            if (checkpoint == null)
             {
-                var member = teamMilestone.Team.ClassMembers
-                    .FirstOrDefault(x => x.StudentId == request.UserId);
-                if (member == null)
+                errors.Add(new OperationError()
+                {
+                    Field = nameof(dto.CheckpointId),
+                    Message = $"No checkpoint with ID: {dto.CheckpointId}",
+                });
+                return;
+            }
+
+            // Get milestone for validation
+            var classEntity = teamMilestone!.Team.Class;
+            var team = teamMilestone.Team;
+
+            // Can not update checkpoint if milestone is evaluated
+            if (teamMilestone.MilestoneEvaluation != null)
+            {
+                errors.Add(new OperationError()
+                {
+                    Field = nameof(request.UpdateDto.CheckpointId),
+                    Message = $"Can not update checkpoint. Reason - The milestone '{teamMilestone.Title}'({teamMilestone.TeamMilestoneId}) has already been evaluated.",
+                });
+                return;
+            }
+
+            // Can not update checkpoint if milestone's status is DONE
+            if (teamMilestone.Status == (int)TeamMilestoneStatuses.DONE)
+            {
+                errors.Add(new OperationError()
+                {
+                    Field = nameof(request.UpdateDto.CheckpointId),
+                    Message = $"Can not update checkpoint. Reason - The milestone '{teamMilestone.Title}'({teamMilestone.TeamMilestoneId}) status is DONE.",
+                });
+                return;
+            }
+
+            // Lecturer have to be assigned to class
+            if (request.UserRole == RoleConstants.LECTURER)
+            {
+                if (classEntity.LecturerId != request.UserId)
                 {
                     errors.Add(new OperationError()
                     {
                         Field = nameof(request.UserId),
-                        Message = $"You ({request.UserId}) are not a member of the team with ID: {teamMilestone.TeamId}"
+                        Message = $"You({request.UserId}) are not the assigned lecturer of class '{classEntity.ClassName}'({classEntity.ClassId}).",
                     });
                     return;
                 }
             }
-            // Check if user is lecturer of class
-            else if (request.UserRole == RoleConstants.LECTURER && teamMilestone.Team.Class.LecturerId != request.UserId)
+            // Student have to be team member
+            else if (request.UserRole == RoleConstants.STUDENT)
             {
-                errors.Add(new OperationError()
+                var isMember = team.ClassMembers
+                    .Any(mem => mem.StudentId == request.UserId);
+                if (!isMember)
                 {
-                    Field = nameof(request.UserId),
-                    Message = $"You ({request.UserId}) are not the assigned lecturer of the class with ID: {teamMilestone.Team.Class.ClassId}"
-                });
-                return;
+                    errors.Add(new OperationError()
+                    {
+                        Field = nameof(request.UserId),
+                        Message = $"You({request.UserId}) are not a member of the team '{team.TeamName}'({team.TeamId})."
+                    });
+                    return;
+                }
             }
 
             // StartDate must be before DueDate
